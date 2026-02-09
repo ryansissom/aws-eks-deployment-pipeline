70112bc4147a1ca6dcb6ada97fbc7d87bf7ee980
User <ryan.sissom@yahoo.com>
2026-02-09 01:35:58 -0600

    RBAC and IAM roles for cluster users and admin

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+# Node Permissions
 resource "aws_iam_role" "eks" {
   name = "${local.env}-${local.eks_name}-eks-cluster"
 
@@ -51,6 +52,98 @@ resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
 resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.nodes.name
+}
+
+
+# AWS Developer
+
+resource "aws_iam_user" "developer" {
+  name = "developer"
+}
+
+resource "aws_iam_policy" "developer_eks" {
+  name = "AmazonEKSDeveloperPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_eks_access_entry" "developer" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_user.developer.arn
+  kubernetes_groups = ["my-viewer"]
+}
+
+
+
+
+# EKS Admin
+
+data "aws_caller_identity" "current" {} # Account Number
+
+resource "aws_iam_role" "eks_admin" {
+  name = "${local.env}-${local.eks_name}-eks-admin"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_admin" {
+  role       = aws_iam_role.eks_admin.name
+  policy_arn = aws_iam_policy.eks_admin.arn
+}
+
+resource "aws_iam_user" "manager" {
+  name = "manager"
+}
+
+resource "aws_iam_policy" "eks_assume_admin" {
+  name = "AmazonEKSAssumeAdminPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["sts:AssumeRole"]
+        Resource = aws_iam_role.eks_admin.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "manager" {
+  user       = aws_iam_user.manager.name
+  policy_arn = aws_iam_policy.eks_assume_admin.arn
+}
+
+# Best practice: use IAM roles due to temporary credentials
+resource "aws_eks_access_entry" "manager" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_role.eks_admin.arn
+  kubernetes_groups = ["my-admin"]
 }
 
 
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,9 +2,9 @@
 
 locals {
   env         = "staging"
-  region      = "us-east-2"
-  zone1       = "us-east-2a"
-  zone2       = "us-east-2b"
+  region      = "us-east-1"
+  zone1       = "us-east-1a"
+  zone2       = "us-east-1b"
   eks_name    = "demo"
   eks_version = "1.34"
 }
