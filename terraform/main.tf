6c4cec095ecb8157381e381ddc0886b5ed022445
User <ryan.sissom@yahoo.com>
2026-03-09 21:42:38 -0500

    Comment out ECR force delete to preserve images for CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ provider "kubernetes" {
 resource "aws_ecr_repository" "my_app_repo" {
   name                 = "my-app"
   image_tag_mutability = "MUTABLE"
-  force_delete         = true
+#  force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
   }
